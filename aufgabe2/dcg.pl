f464e2394a968c0f94004e4867bac63335d046d6
blub <yavuz.arslan@haw-hamburg.de>
2016-11-30 19:48:53 +0100

    aufg. 2 fertig

--- aufgabe2/dcg.pl
+++ aufgabe2/dcg.pl
@@ -29,7 +29,13 @@ frage -->
         vp(SemVP,SemNP,SemPP,Num,Gen),
         [?],
         {
-        solve_ergaenz(SemNP,SemPP)}.
+        Call =.. [SemNP,SemPP, X],
+        findall(X, call(Call), Z),
+        (
+        length(Z,0), keine_loesung; %keine Loesung
+        length(Z,1), ergaenz_antwort(SemNP,SemPP,Z); %eine Loesung
+        ergaenz_antwort_pl(SemNP,SemPP,Z)) %mehrere Loesungen
+        }.
         
 % Entscheidungsfrage 1 (ist william der bruder von john?)
 frage -->
@@ -38,27 +44,26 @@ frage -->
         np(SemNP2,SemPP2,Num,Gen),
         [?],
         {
-        solve_entsch(SemNP1,SemNP2,SemPP2)
+        Call =.. [SemNP2,SemPP2,SemNP1],
+        (call(Call),
+        writeln("Ja."));
+        writeln("Nein.")
         }.
         
 % Entscheidungsfrage 2 (wie viele brueder hat jack?)
 frage -->
-      i(_SemI, Num),
-      
-        
-solve_ergaenz(SemNP,SemPP) :-
-        Call =.. [SemNP,SemPP, X],
-        findall(X, call(Call), Z),
-        (
-        length(Z,0), keine_loesung; %keine Loesung
-        length(Z,1), ergaenz_antwort(SemNP,SemPP,Z); %eine Loesung
-        ergaenz_antwort_pl(SemNP,SemPP,Z)). %mehrere Loesungen
-        
-solve_entsch(SemNP1,SemNP2,SemPP2) :-
-        Call =.. [SemNP2,SemPP2,SemNP1],
-        (call(Call),
-        writeln("Ja."));
-        writeln("Nein.").
+      a(_SemA),
+      pr(_SemPr,Num),
+      vp(_SemVP,SemNP1,_SemPP1,Num,Gen),
+      np(SemNP2,_SemPP2,Num,Gen),
+      [?],
+      {
+      Call =.. [SemNP1,SemNP2,X],
+      findall(X, call(Call), Z),
+      (length(Z,0), entsch_antwort(0,SemNP1,SemNP2,Z); %keine Loesung
+      length(Z,1), entsch_antwort(1,SemNP1,SemNP2,Z); %eine Loesung
+      entsch_antwort(2,SemNP1,SemNP2,Z)) %mehrere Loesungen
+      }.
 
 vp(SemVP,_SemNP,_SemPP,Num,_Gen) -->
          v(SemVP,Num).
@@ -67,15 +72,20 @@ vp(SemVP,SemNP,SemPP,Num,Gen) -->
          v(SemVP,Num),
          np(SemNP,SemPP,Num,Gen).
          
+
+vp(SemVP,SemNP,_SemPP,Num,_Gen) -->
+         n(SemNP,Num,Gen),
+         v(SemVP,Num).
+         
 np(SemNP,_SemPP,_Num,_Gen) -->
          pn(SemNP).
          
 np(SemNP,_SemPP,Num,Gen) -->
-         a(_SemA,Num,Gen),
+         det(_SemDet,Num,Gen),
          n(SemNP,Num,Gen).
          
 np(SemNP,SemPP,Num,Gen) -->
-         a(_SemA,Num,Gen),
+         det(_SemDet,Num,Gen),
          n(SemNP,Num,Gen),
          pp(SemPP,Num,Gen).
 
@@ -83,9 +93,11 @@ pp(SemPP,Num,Gen) -->
          p(_SemP),
          np(SemPP,_SemPP,Num,Gen).
 
-a(SemA,Num,Gen) --> [X], {lex(X,SemA,a,Num,Gen)}.
-i(SemI,Num) --> [X], {lex(X,SemI,i,Num,_)}.
-v(SemV,Num) --> [X], {lex(X,SemV,v,Num,_)}.
-p(SemP) --> [X], {lex(X,SemP,p,_,_)}.
-pn(SemN) --> [X], {lex(X,SemN,pn,_,_)}.
+det(SemDet,Num,Gen) --> [X], {lex(X,SemDet,det,Num,Gen)}.
+i(SemI,Num) --> [X], {lex(X,SemI,i,Num,_Gen)}.
+v(SemV,Num) --> [X], {lex(X,SemV,v,Num,_Gen)}.
+p(SemP) --> [X], {lex(X,SemP,p,_Num,_Gen)}.
+pn(SemN) --> [X], {lex(X,SemN,pn,_Num,_Gen)}.
 n(SemN,Num,Gen) --> [X], {lex(X,SemN,n,Num,Gen)}.
+a(SemA) --> [X], {lex(X,SemA,a,_Num,_Gen)}.
+pr(SemPr,Num) --> [X], {lex(X,SemPr,pr,Num,_Gen)}.
